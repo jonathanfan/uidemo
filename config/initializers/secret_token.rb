4f075577755c5c4def16fc8ef0bb68f5f1ce5e5b
Jonathan Fan <jonathan.fan@gmail.com>
2011-09-14 01:55:32 -0700

    nothing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uidemo::Application.config.secret_token = '876535cc06d76af34b2a560c8ce224b5e5430be77f50ce146c49c56ecf44daded1351b1f685132ecd62ede06281c318e344a003a7f6fbe54f31bc85f8f57275e'
